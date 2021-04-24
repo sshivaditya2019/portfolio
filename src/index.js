7b4c1b8a1c48dd329e07397c984db85db903e8a8
Shivaditya Shivganesh <shivaditya.shivganesh2019@vitstudent.ac.in>
2021-04-24 14:06:17 +0530

    Added Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import HelloWorld from './App';
 
-const HelloWorld = () => {
-    return (
-        <h1>
-            Hello World
-        </h1>
-    );
-}
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
